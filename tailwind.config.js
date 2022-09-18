b53a1c983b73f91ae51b48f1db869d26b1ab79d8
iamzilfa <zilcyam@gmail.com>
2022-09-18 23:22:38 +0200

    all

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        brightOrange: '#F53838',
+        lightOrange: '#FFECEC',
+        GreyLight: '#DDDDDD',
+        theLightGrey:'#F8F8F8',
+        bodyColor:'#F6F6F6',
+      },
+    },
+  },
+  plugins: [],
+}
